52457bcb8adb9f095de8571caa72ca913d89fe47
gh-lenny <Leonardo1147@outlook.it>
2024-11-07 19:27:37 +0100

    Transizione tra scene aggiunta e impostazione iniziale dei tile set

--- gioco/script/scene_switch.gd
+++ gioco/script/scene_switch.gd
@@ -4,4 +4,6 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: CharacterBody2D) -> void:
+	Transition.transition()
+	await Transition.on_transition_finished
 	get_tree().change_scene_to_file("res://levels/level2.tscn")
